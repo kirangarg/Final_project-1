541d6c4bd7b963b2b2468d47d1df7d6fbeaf4d14
Sandeepdogra <sandeepdogra6494@gmail.com>
2017-07-20 22:50:47 +0530

    signup created

--- upload/demoupload/models.py
+++ upload/demoupload/models.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here
-class User(models.Model):
-    name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=255)
-    age = models.IntegerField(default=0)
-    verified_phone = models.BooleanField(default=False)
+class UserModel(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField()
+    user_name = models.CharField(max_length=120)
+    password = models.CharField(max_length=40)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
--- upload/upload/views.py
+++ upload/upload/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
 def signup_view(request):
     #logic
