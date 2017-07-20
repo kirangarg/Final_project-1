12fe805950d2cfdd2b392e739695da211a4e3cca
Sandeepdogra <sandeepdogra6494@gmail.com>
2017-07-20 12:01:33 +0530

    django models demo created

--- upload/demoupload/models.py
+++ upload/demoupload/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    age = models.IntegerField(default=0)
+    verified_phone = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
